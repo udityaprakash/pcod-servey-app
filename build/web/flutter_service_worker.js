46b379c39cd3a1e099096f55f7b7dd1d374f7bd8
Uditya Prakash <udityaprakash983@gmail.com>
2025-02-08 15:11:52 +0530

    rebuild the web build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d61a3fef86cd499f5b17e19224d50234
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "107f09d918f7eb57399e193772032081",
+"flutter_bootstrap.js": "ff9e65044517c4db7756e4077cd49358",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
